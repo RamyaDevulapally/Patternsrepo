5cfd82389d1fd885a579175700e7c488a7b622df
Ramya Devulapally <S547918@nwmissouri.edu>
2023-04-29 23:32:18 -0500

    changes

---
+++ src/main/java/com/nwmissouri/edu/DMS/repository/TrainerRepository.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.nwmissouri.edu.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.nwmissouri.edu.DMS.models.Trainer;
+
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer>  {
+
+}
